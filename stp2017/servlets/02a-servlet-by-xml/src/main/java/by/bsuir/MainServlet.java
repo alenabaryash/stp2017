aab2279a5836bfd6294ea31024be5b86326dc70b
Alena <alenabaryash@gmail.com>
2017-11-19 18:12:47 +0200

    Added 02c-servlet-by-xml-my

--- stp2017/servlets/02a-servlet-by-xml/src/main/java/by/bsuir/MainServlet.java
+++ stp2017/servlets/02a-servlet-by-xml/src/main/java/by/bsuir/MainServlet.java
@@ -1,14 +1,11 @@
 package by.bsuir;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/stp",
-            loadOnStartup = 1)
 public class MainServlet extends HttpServlet {
 
     @Override
