46c7bce1aabb8eb37b8f4bbf5f969ca480d29147
Alena <alenabaryash@gmail.com>
2017-11-27 19:30:09 +0300

    Some changes;

--- stp2017/servlets/02a-servlet-by-xml/src/main/java/by/bsuir/MainServlet.java
+++ stp2017/servlets/02a-servlet-by-xml/src/main/java/by/bsuir/MainServlet.java
@@ -10,6 +10,6 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("index.jsp").forward(req, resp);
+        req.getRequestDispatcher("index.html").forward(req, resp);
     }
 }
